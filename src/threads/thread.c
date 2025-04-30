4347d65ac07262e1a8c062befaf517c697483157
axel0224 <alm100620@gmail.com>
2025-04-30 08:29:09 -0500

    take 2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -291,10 +291,10 @@ thread_create (const char *name, int priority,
        ASSERT (t->status == THREAD_BLOCKED);
    
        if (thread_mlfqs) {
-        enum intr_level old_level = intr_disable();
-        // This will refresh recent_cpu in case it became stale while blocked
-        update_recent_cpu_all();
-        intr_set_level(old_level);
+        // enum intr_level old_level = intr_disable();
+        // // This will refresh recent_cpu in case it became stale while blocked
+        // update_recent_cpu_all();
+        // intr_set_level(old_level);
     
         thread_update_priority(t);
     }
